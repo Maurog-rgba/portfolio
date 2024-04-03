f28805c615191d6794e7c5ba30bba54e50f49400
Maurog-rgba <gabriel.mauro.santos.3@gmail.com>
2024-04-03 11:03:46 -0300

    Feat: Add typing animation to header component

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  text = 'Frontend Developer.';
+  currentText = '';
+  index = 0;
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.typeText();
+  }
+
+  typeText() {
+    if (this.index < this.text.length) {
+      this.currentText += this.text.charAt(this.index);
+      this.index++;
+      setTimeout(() => {
+        this.typeText();
+      }, 100);
+    }
+  }
 }
