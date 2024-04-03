a731bc3e28542eae6c5c237f3486480aeb8db883
Maurog-rgba <gabriel.mauro.santos.3@gmail.com>
2024-04-03 10:08:35 -0300

    Refactor: projects component layout and add project stack images

--- src/app/shared/components/projects/projects.component.ts
+++ src/app/shared/components/projects/projects.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
@@ -14,21 +15,36 @@ export class ProjectsComponent {
       description: 'A simple color palette generator.',
       github: 'https://github.com/Maurog-rgba/color-flipper',
       live: 'https://folor-clipper.netlify.app/src/app/palette/palette.html',
-      image: 'https://placehold.co/600x400/000000/FFFFFF/png',
+      image: '../../../../assets/images/projects/color-flipper.png',
+      stack:
+        'https://icongr.am/devicon/javascript-original.svg?size=148&color=currentColor',
     },
     {
       name: 'Porsche Brasil',
       description: 'A clone of the Porsche Brasil website.',
       github: '',
       live: '',
-      image: 'https://placehold.co/600x400/000000/FFFFFF/png',
+      image: '../../../../assets/images/projects/porsche-brasil.png',
+      stack:
+        'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
     },
     {
       name: 'Todo List',
       description: 'A simple todo list app with local storage support.',
-      github: '',
+      github: 'https://github.com/Maurog-rgba/nodejs-todo-list',
       live: '',
-      image: 'https://placehold.co/600x400/000000/FFFFFF/png',
+      image: '../../../../assets/images/projects/todo-list.png',
+      stack:
+        'https://icongr.am/devicon/nodejs-original.svg?size=148&color=currentColor',
+    },
+    {
+      name: 'QR Code Component',
+      description: 'A simple QR code component.',
+      github: 'https://github.com/Maurog-rgba/qr-code-component',
+      live: 'https://main--solutionqrcodecomponent.netlify.app',
+      image: '../../../../assets/images/projects/qrcode-component.png',
+      stack:
+        'https://icongr.am/devicon/html5-original.svg?size=148&color=currentColor',
     },
   ];
 }
