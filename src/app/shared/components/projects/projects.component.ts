8b2e34aaac930ac44f438c6a3cac02542a4b0fb9
Maurog-rgba <gabriel.mauro.santos.3@gmail.com>
2024-04-08 11:51:46 -0300

    Update header component to fix typo and add LinkedIn link

--- src/app/shared/components/projects/projects.component.ts
+++ src/app/shared/components/projects/projects.component.ts
@@ -57,8 +57,6 @@ export class ProjectsComponent implements OnInit {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           entry.target.classList.add('show');
-        } else {
-          entry.target.classList.remove('show');
         }
       });
     });
